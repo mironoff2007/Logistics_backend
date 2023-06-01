6a3df4aaccd9a72370f93654eec4ea4ffd393f8a
Mironov Ury <mironoff2007@rambler.ru>
2023-06-01 09:42:37 +0300

    add routing

--- src/main/kotlin/ru/logistics/plugins/Routing.kt
+++ src/main/kotlin/ru/logistics/plugins/Routing.kt
@@ -3,9 +3,29 @@ package ru.logistics.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import ru.logistics.security.authenticate
+import ru.logistics.security.hashing.HashingService
+import ru.logistics.security.signIn
+import ru.logistics.security.signUp
+import ru.logistics.security.token.TokenConfig
+import ru.logistics.security.token.TokenService
 
-fun Application.configureRouting() {
+fun Application.configureRouting(
+    //userDataSource: UserDataSource,
+    hashingService: HashingService,
+    tokenService: TokenService,
+    tokenConfig: TokenConfig
+) {
     routing {
+        signIn(//userDataSource,
+            hashingService,
+            tokenService,
+            tokenConfig)
+        signUp(
+            hashingService,
+            //userDataSource
+        )
+        authenticate()
         get("/") {
             call.respondText("Hello World!")
         }
