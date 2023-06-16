18c54b40058eaa4cf256fd9d7b6cd9c587710a10
Mironov Ury <mironoff2007@rambler.ru>
2023-06-16 15:06:20 +0300

    update ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val exposed_version : String by project
 plugins {
     kotlin("jvm") version "1.8.21"
     id("io.ktor.plugin") version "2.3.0"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
 }
 
 group = "ru.logistics"
